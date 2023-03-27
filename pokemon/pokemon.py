de0248eb86ffa9990f48b3c63969aced798e3b58
henriquexr <126699873+henriquexr@users.noreply.github.com>
2023-03-27 15:53:28 -0300

    Add files via upload

---
+++ pokemon/pokemon.py
@@ -0,0 +1,11 @@
+class Pokemon:
+    def __init__(self, nome, tipo, ataque, defesa, hp, level):
+        self._nome = nome
+        self._tipo = tipo
+        self._hp = ataque
+        self._level = defesa
+        self._hp = hp
+        self._level = level
+
+
+
